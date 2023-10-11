f1aac9cfe1fc572f3dacee25a4e3f75dd1c6e173
Theeraporn <63072072+heroclik@users.noreply.github.com>
2023-10-12 01:32:47 +0700

    add login

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
 const client = require('../configs/databasepg.js');
+const jwt =  require('jsonwebtoken');
+const { jwtSecret } = require('../configs/jwt.js');
 
 // Register a new user
 async function register(req, res) {
@@ -23,4 +25,39 @@ async function register(req, res) {
     }
 }
 
-module.exports = { register };
+// login
+async function login(req, res) {
+    const { username, password } = req.body;
+
+    // Check if the user with the provided username or email exists in the database
+    const query = `
+        SELECT user_id, username, password_hash
+        FROM users
+        WHERE username = $1`;
+
+    try {
+        const result = await client.query(query, [username]);
+
+        if (result.rows.length === 0) {
+            return res.status(401).json({ message: 'Authentication failed' });
+        }
+
+        const user = result.rows[0];
+
+        // Compare the provided password with the stored hashed password
+        const passwordMatch = await bcrypt.compare(password, user.password_hash);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Authentication failed' });
+        }
+
+        // Generate and send a JSON Web Token (JWT) for authentication
+        const token = jwt.sign({ user_id: user.user_id, username: user.username }, jwtSecret);
+        res.status(200).json({ message: 'Authentication successful', token });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+module.exports = { register, login };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,10 +2,68 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controller/auth.controller.js');
 
-// Register a new user
+/**
+ * @swagger
+ * /user/register:
+ *   post:
+ *     summary: Register a new user
+ *     description: Create a new user account by providing user information.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *     responses:
+ *       201:
+ *         description: User registration successful. Returns the user's ID.
+ *       400:
+ *         description: Bad request (e.g., missing or invalid input data)
+ *       500:
+ *         description: Internal server error
+ */
+
 router.post('/register', authController.register);
 
-// User login
-// router.post('/login', authController.login);
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: User login
+ *     description: Authenticate a user by providing valid credentials (username or email and password).
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - username
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Authentication successful. Returns a JWT token.
+ *       401:
+ *         description: Authentication failed. Invalid username or password.
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/login', authController.login);
 
 module.exports = router;
